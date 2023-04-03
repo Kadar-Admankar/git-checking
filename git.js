942f93e7280a52110c7d93d867420411a131b36b
Kadar <admankarkadar@sggs.ac.in>
2023-04-03 20:37:54 +0530

    all files committed

---
+++ git.js
@@ -0,0 +1,7 @@
+// mkdir <folderName>     // new folder created
+// cd <folderName>      // folder changed to working folder
+//cd..     // one folder back
+// git clone // cloning repository in separate folder
+// git checkout -b <branchName>   // creating new branch from main branch
+//git init  // to initialize empty git repository i.e.  .git file
+//In linux/mac any file that starts with . is hidden for exa - .git
